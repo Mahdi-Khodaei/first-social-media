6dcc2941bb62492fdc0418574072a8574573c9bf
MahdiKhodaei <khodaei82.mahdi@gmail.com>
2024-09-03 10:20:03 +0330

    add account commit

--- A/account/views.py
+++ A/account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-
+from .forms import UserRegisterForm
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'account/register.html')
+        form = UserRegisterForm()
+        return render(request, 'account/register.html', {"form": form})
 
     def post(self,request):
         pass
--- A/home/urls.py
+++ A/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 
+
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
 ]
